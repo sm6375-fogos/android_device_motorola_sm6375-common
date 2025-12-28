3d6cf2b0d3ce0e0d0aa0ba8a5eab46218b153b1c
Bruno Martins <bgcngm@gmail.com>
2025-12-28 12:52:09 +0100

    sm6375-common: Use common libqti-perfd-client and power-libperfmgr Change-Id: I22d6b35c72629553817005deaefcae60abc72cfd

--- common.mk
+++ common.mk
@@ -273,7 +273,8 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service.lineage-libperfmgr \
+    libqti-perfd-client
 
 # Properties
 PRODUCT_VENDOR_PROPERTY_BLACKLIST := \
@@ -292,7 +293,9 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/motorola \
     hardware/google/interfaces \
-    hardware/google/pixel
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
+    hardware/qcom-caf/common/libqti-perfd-client
 
 # Telephony
 PRODUCT_PACKAGES += \
